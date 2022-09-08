b2e1cad920b929eebf5a58b6d6489d27eef263fd
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-08 18:56:32 -0300

    Desafio 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,16 @@
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, {
+  $currentDate: {
+    ultimaModificacao: true,
+  },
+});
+
+db.produtos.find({
+  ultimaModificacao: {
+    $exists: true,
+  },
+}, {
+  _id: false,
+  nome: true,
+});
